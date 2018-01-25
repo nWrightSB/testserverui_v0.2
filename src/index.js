61d410383a83c2e1585ee03bc1500d5385eee8ea
nWrightSB <nathan.wright@smartbear.com>
2018-01-25 16:24:42 -0500

    NavLink added as a module (change folder name)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import './index.css';
 import App from './components/app/App';
+import Analyze from './components/analyze/Analyze';
+import Run from './components/run/Run';
+import History from './components/history/History'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <Router history={hashHistory}>
-        <Route path="/" component={App}/>
+        <Route path="/" component={App}>
+            <Route path="/analyze" component={Analyze}/>
+            <Route path="/run" component={Run}/>
+            <Route path="/history" component={History}/>
+        </Route>
     </Router>
 ), document.getElementById('root'));
+
 registerServiceWorker();
