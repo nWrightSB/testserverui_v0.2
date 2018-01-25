d4da84f4312d07d83192b2d335f0d54e51c0cd64
nWrightSB <nathan.wright@smartbear.com>
2018-01-25 11:17:50 -0500

    added routing to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import './index.css';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}/>
+    </Router>
+), document.getElementById('root'));
 registerServiceWorker();
