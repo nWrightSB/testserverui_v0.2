277f92d0d262a6d83a1eee413c9e43d3af4ec9ce
nWrightSB <nathan.wright@smartbear.com>
2018-02-21 12:01:40 -0500

    working on adding login modal and fields to the ts_ui

--- src/elements/loginbutton/LoginButton.js
+++ src/elements/loginbutton/LoginButton.js
@@ -17,6 +17,8 @@ const LoginButton = props => {
   function handleEvent(event) {
     // SHOULD BRING UP LOGIN MODAL
     if (!currentUser) {
+      let loginFieldsContainer = document.getElementById('login-fields-container')
+      loginFieldsContainer.style.display = "block"
       props.handleLogin("testUser", "testPass")
     } else {
       props.handleLogout()
@@ -24,9 +26,17 @@ const LoginButton = props => {
   }
   
   return (
-    <div className="login-button-container">
-      <div onClick={() => handleEvent()} className="login-button">{buttonText}</div>
-      <i className={buttonIcon} aria-hidden="true"></i>
+    <div className="login-element-container">
+      <div className="login-fields-container" id="login-fields-container">
+        <form className="login-fields">
+          <input type="text" name="username" id="login-field-username" placeholder="username" className="login-field" />
+          <input type="text" name="password" id="login-field-username" placeholder="password" className="login-field" />
+        </form>
+      </div>
+      <div className="login-button-container">
+        <div onClick={() => handleEvent()} className="login-button">{buttonText}</div>
+        <i className={buttonIcon} aria-hidden="true"></i>
+      </div>
     </div>
   )
 }
